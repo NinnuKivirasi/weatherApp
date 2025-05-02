bb59cdb6c522849f8ef63605f80bfa3c6d85a835
Ninnu Kivirasi <nina.kivirasi@student.laurea.fi>
2025-05-02 14:34:23 +0300

    toast added

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,74 +1,94 @@
 import {
-    IonButton,
-    IonButtons,
-    IonCard,
-    IonCardContent,
-    IonContent, 
-    IonHeader,
-    IonInput,
-    IonInputPasswordToggle,
-    IonList,
-    IonPage, 
-    IonTitle, 
-    IonToolbar } from '@ionic/react';
+  IonButton,
+  IonButtons,
+  IonCard,
+  IonCardContent,
+  IonContent,
+  IonHeader,
+  IonInput,
+  IonInputPasswordToggle,
+  IonList,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonToast
+} from '@ionic/react';
 import './Home.css';
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
 import { loginUser } from '../firebaseConfig';
 
 const Login: React.FC = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
-  
-    async function login() {
-      const res = await loginUser(username, password)
-      if (res){
-        console.log('You have logged in!')
-      }
+  const [toastMessage, setToastMessage] = useState('');
+  const [showToast, setShowToast] = useState(false);
+
+  async function login() {
+    const res = await loginUser(username, password);
+    if (res) {
+      setToastMessage('Login success!');
+      setShowToast(true);
+    
+    } else {
+      setToastMessage('Failed to login.');
+      setShowToast(true);
     }
-      
-  
-      return (
-          <>
-            <IonPage id="main-content">
-              <IonHeader>
-                <IonToolbar>
-                  <IonButtons slot="start">
-                  </IonButtons>
-                  <IonTitle>Login</IonTitle>
-                </IonToolbar>
-              </IonHeader>
-              <IonContent className="ion-padding">
-              <IonCard>
-              <IonCardContent text-center>
+  }
+
+  return (
+    <>
+      <IonPage id="main-content">
+        <IonHeader>
+          <IonToolbar>
+            <IonButtons slot="start"></IonButtons>
+            <IonTitle>Login</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent className="ion-padding">
+          <IonCard>
+            <IonCardContent text-center>
               <IonList>
-              <IonInput 
+                <IonInput
                   placeholder="Username"
                   value={username}
-                  onIonChange={(e: any) => 
-                  setUsername(e.target.value)}/>
-  
-                  <IonInput 
-                    type="password"
-                    value={password}
-                    placeholder="Password"
-                    onIonChange={(e: any) => 
-                    setPassword(e.target.value)}>
-                    <IonInputPasswordToggle slot='end'/>
-                    </IonInput>
+                  onIonChange={(e: any) => setUsername(e.target.value)}
+                />
+
+                <IonInput
+                  type="password"
+                  value={password}
+                  placeholder="Password"
+                  onIonChange={(e: any) => setPassword(e.target.value)}
+                >
+                  <IonInputPasswordToggle slot="end" />
+                </IonInput>
               </IonList>
-              <IonButton onClick={login} routerLink='./home' expand='block'> Login
-            </IonButton>
+              <IonButton onClick={login} expand="block">
+                Login
+              </IonButton>
               <p>
-              Need to register first? Go <Link to="./register">Register</Link>
+                Need to register first? Go <Link to="./register">Register</Link>
               </p>
             </IonCardContent>
-            </IonCard>
-              </IonContent>
-            </IonPage>
-          </>
-        );
-      }
-  
-  export default Login;
+          </IonCard>
+        </IonContent>
+      </IonPage>
+
+      {/* TOAST-VIESTI */}
+      <IonToast
+        isOpen={showToast}
+        onDidDismiss={() => setShowToast(false)}
+        message={toastMessage}
+        duration={2000}
+        position="top"
+        color={toastMessage.includes('success') ? 'success' : 'danger'}
+        
+      />
+
+    </>
+  );
+};
+
+export default Login;
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,15 +1,17 @@
 import { 
-    IonButton,
-    IonButtons,
-    IonCard, 
-    IonCardContent,
-    IonContent,
-    IonInput,
-    IonInputPasswordToggle,
-    IonList,
-    IonPage,
-    IonTitle,
-    IonToolbar,
+  IonButton,
+  IonButtons,
+  IonCard, 
+  IonCardContent,
+  IonContent,
+  IonInput,
+  IonInputPasswordToggle,
+  IonList,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonToast,
+  IonIcon
 } from '@ionic/react';
 import './Home.css';
 import { Link } from 'react-router-dom';
@@ -17,72 +19,93 @@ import { useState } from 'react';
 import { registerUser } from '../firebaseConfig';
 
 const Register: React.FC = () => {
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
-    const [cpassword, setCPassword] = useState('')
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [cpassword, setCPassword] = useState('');
 
-    async function register() {
-        if (password !== cpassword) {
-          return console.log('Passwords do not match')
-        }
-        if(username.trim() === '' || password.trim() === '') {
-          return console.log('Username and password are required')
-        }
-  
-        const res = await registerUser(username, password)
-        if(res) {
-          console.log('You have registered successfully!')
-        }
-      }
+  const [toastMessage, setToastMessage] = useState('');
+  const [showToast, setShowToast] = useState(false);
+
+  async function register() {
+    if (password !== cpassword) {
+      setToastMessage(' Passwords do not match');
+      setShowToast(true);
+      return;
+    }
+    if (username.trim() === '' || password.trim() === '') {
+      setToastMessage('Username and password required');
+      setShowToast(true);
+      return;
+    }
+
+    const res = await registerUser(username, password);
+    if (res) {
+      setToastMessage('Registration successful!');
+    } else {
+      setToastMessage('Registration failed');
+    }
+    setShowToast(true);
+  }
 
   return (          
-    <IonPage id="main-content">
-            <IonToolbar>
-                <IonButtons slot="start">
-                </IonButtons>
-                <IonTitle>Register</IonTitle>
-            </IonToolbar>
+    <>
+      <IonPage id="main-content">
+        <IonToolbar>
+          <IonButtons slot="start" />
+          <IonTitle>Register</IonTitle>
+        </IonToolbar>
+
         <IonContent className="ion-padding">
-        <IonCard>
-            <IonCardContent >
-            <IonList>
-  
-            <IonInput 
-            placeholder="Username"
-            value={username}
-            onIonChange={(e: any) => 
-            setUsername(e.target.value)}/>
+          <IonCard>
+            <IonCardContent>
+              <IonList>
 
-            <IonInput 
-            type="password"
-            value={password}
-            placeholder="Password"
-            onIonChange={(e: any) => 
-            setPassword(e.target.value)}>
-            <IonInputPasswordToggle slot='end'/>
-            </IonInput>
+                <IonInput 
+                  placeholder="Username"
+                  value={username}
+                  onIonChange={(e: any) => setUsername(e.target.value)}
+                />
 
-            <IonInput 
-            type="password"
-            value={cpassword}
-            placeholder="Confirm password"
-            onIonChange={(e: any) => 
-            setCPassword(e.target.value)}>
-            <IonInputPasswordToggle slot='end'/>
-            </IonInput> 
+                <IonInput 
+                  type="password"
+                  value={password}
+                  placeholder="Password"
+                  onIonChange={(e: any) => setPassword(e.target.value)}
+                >
+                  <IonInputPasswordToggle slot='end'/>
+                </IonInput>
 
-            <IonButton expand='block' onClick={register}>Register</IonButton>
+                <IonInput 
+                  type="password"
+                  value={cpassword}
+                  placeholder="Confirm password"
+                  onIonChange={(e: any) => setCPassword(e.target.value)}
+                >
+                  <IonInputPasswordToggle slot='end'/>
+                </IonInput> 
+
+                <IonButton expand='block' onClick={register}>Register</IonButton>
 
                 <p>
-                You have account already? <Link to="./login">Login</Link>
+                  You have an account already? <Link to="./login">Login</Link>
                 </p>
 
-            </IonList>
-
+              </IonList>
             </IonCardContent>
-        </IonCard>
+          </IonCard>
         </IonContent>
-    </IonPage>
-    );
-  }
+      </IonPage>
+
+      <IonToast
+        isOpen={showToast}
+        onDidDismiss={() => setShowToast(false)}
+        message={toastMessage}
+        duration={2000}
+        position="top"
+        color={toastMessage.includes('success') ? 'success' : 'danger'}
+      />
+    </>
+  );
+};
+
 export default Register;
