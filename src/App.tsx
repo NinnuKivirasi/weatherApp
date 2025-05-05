86b94bb9d3cd21738c2b6a262a3ee2b4aa2fa20f
Viljami V. <viljami.j.viinikainen@gmail.com>
2025-05-05 21:40:14 +0300

    Update App.tsx
    IMPORT IONSPLITPANE

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { Redirect, Route } from 'react-router-dom';
-import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
+import { IonApp, IonRouterOutlet, setupIonicReact, IonSplitPane } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import { Capacitor } from '@capacitor/core';
 import { SplashScreen } from '@capacitor/splash-screen';
